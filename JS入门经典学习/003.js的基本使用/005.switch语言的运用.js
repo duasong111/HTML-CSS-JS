6e341f0df355f6b25d44b60a2a91d5cb11a6c265
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-03-13 22:01:34 +0800

    Create 005.switch语言的运用.js

---
+++ JS入门经典学习/003.js的基本使用/005.switch语言的运用.js
@@ -0,0 +1,23 @@
+//不用怕，简单
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>js</title>
+    </head>
+    <body>
+        <script>
+     var now = new Date();
+     var hour = now.getHours();
+     switch (hour)
+     {
+         case 13 :
+         case 14 :
+             case 15 : alert("今天天气真好啊  "); break;
+             case 16 : 
+             case 17 :
+                 case 18 : alert("再写一百行代码再休息！！"); break;
+                 default : alert("its over "); break;
+     }  //其实这个和那个c语言的是一摸一样的,怪不得我一下午学习了一章呢、
+     </script>
+    </body>
+</html>
