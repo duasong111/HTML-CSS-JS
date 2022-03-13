75b54e8f26d6e90fdf9ba338df3a474f477385b7
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-03-13 22:02:52 +0800

    Create 006.while语句的运用.js

---
+++ JS入门经典学习/003.js的基本使用/006.while语句的运用.js
@@ -0,0 +1,23 @@
+//似曾相识的感觉
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>js</title>
+    </head>
+    <body>
+        <script>
+     var now = new Date();
+     var hour = now.getHours();
+     switch (hour)
+     {
+         case 13 :
+         case 14 :
+             case 15 : alert("今天天气真好啊  "); break;
+             case 16 : 
+             case 17 :
+                 case 18 : alert("再写一百行代码再休息！！"); break;
+                 default : alert("its over "); break;
+     }
+     </script>
+    </body>
+</html>
