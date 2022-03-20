ce5aa5ea9b21db0b669c146111a256b6c24d3548
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-03-20 21:55:25 +0800

    Create 004.二维数组使用.js

---
+++ JS入门经典学习/003.语句_数组_对象/004.二维数组使用.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>js</title>
+    </head>
+    <body>
+        <h4>姓名 高数 计算机</h4>
+       <script >
+       var stus = new Array();
+       var  i, j;
+       stus [0] = new Array("刘涛",78,93);
+       stus [1] = new Array("赵四",43,33);
+       stus [2] = new Array("张三",72,65);
+       for(i=0;i<stus.length;i++){
+           for(j=0;j<stus[i] .lengthth;j++){
+           document.write(stus[i] [j]+"&nbsp;&nbsp;&nbsp;");
+        }
+        document.write("<br>");
+       }          
+          
+       </script>
+      
+    </body>
+</html>
