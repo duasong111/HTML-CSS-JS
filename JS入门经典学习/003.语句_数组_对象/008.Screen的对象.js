d064c8f120d90e5956a57d18c0ef6238d9717616
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-03-20 21:58:45 +0800

    Create 008.Screen的对象.js

---
+++ JS入门经典学习/003.语句_数组_对象/008.Screen的对象.js
@@ -0,0 +1,17 @@
+//为啥我这个没有啥作用啊
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>js</title>
+    </head>
+    <body>
+       <script> 
+       var w;
+       w = window.screen.width; //哪里出现了问题呢？？
+       document.write("屏幕的宽度是:"+W+"<br>");
+       h=window.screen.height;
+       document.height("这个屏幕的高度是:"+h+"<br>");
+       </script>
+      
+    </body>
+</html>
